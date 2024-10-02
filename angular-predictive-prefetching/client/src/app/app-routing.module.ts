97a88ef109c2e7294018ce3b6cf83d341ccd2312
yuriy.vasilev <yuriy.vasilev@rtkit.ru>
2024-10-02 22:33:11 +0500

    clean

--- angular-predictive-prefetching/client/src/app/app-routing.module.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { MerchDisplayComponent } from './merch-display/merch-display.component';
-
-export const routes: Routes = [
-  { path: '', component: MerchDisplayComponent },
-  { path: ':category', component: MerchDisplayComponent },
-];
