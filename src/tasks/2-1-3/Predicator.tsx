89e2f1cdd039a59e8cc1f8a89a0d99ba2fd1113c
yuriy.vasilev <yuriy.vasilev@rtkit.ru>
2024-10-06 19:40:36 +0500

    2.1 end

--- src/tasks/2-1-3/Predicator.tsx
+++ src/tasks/2-1-3/Predicator.tsx
@@ -10,6 +10,8 @@ export const Predicator = () => {
   const [isCompute, setIsCompute] = useState(false);
   const [resultComputed, setResultComputed] = useState("No result");
   const [resultTest, setResultTest] = useState("No test");
+  const [controlDataModel, setControlDataModel] = useState("");
+  const [model, setModel] = useState<tf.Sequential | undefined>();
 
   tf.util.shuffle(trainData.timeSec);
 
@@ -28,33 +30,39 @@ export const Predicator = () => {
   }
 
   const handleClick = () => {
-    const model = createLinearModel();
-    createLinearRegression(model);
-    testingModel(model);
-    modelForPredict(model);
+    createLinearModel();
+    if (model) {
+      createLinearRegression(model).then(() => {
+        modelForPredict(model);
+      });
+    }
+  };
+
+  const handleTesting = () => {
+    if (model) {
+      controlTestingModel(model);
+    }
   };
 
   const createLinearModel = () => {
     const model = tf.sequential();
     model.add(tf.layers.dense({ inputShape: [1], units: 1 }));
     model.compile({ optimizer: "sgd", loss: "meanAbsoluteError" });
-    return model;
+    setModel(model);
   };
 
-  const testingModel = (model: tf.Sequential) => {
-    const test = model.evaluate(
-      getTestTensors().sizeMB,
-      getTestTensors().timeSec,
-    );
-    console.log({ test });
-    setResultTest(JSON.stringify(test));
+  const controlTestingModel = (model: tf.Sequential) => {
+    const test = model
+      .evaluate(getTestTensors().sizeMB, getTestTensors().timeSec)
+      .toString();
+    setControlDataModel(test);
   };
 
   const createLinearRegression = async (model: tf.Sequential) => {
     setIsCompute(true);
     const result = await model
       .fit(getTrainTensors().sizeMB, getTrainTensors().timeSec, {
-        epochs: 1000,
+        epochs: 200,
       })
       .finally(() => setIsCompute(false));
     return result;
@@ -64,9 +72,9 @@ export const Predicator = () => {
     const smallFileMB = 1;
     const bigFileMB = 100;
     const hugeFileMB = 10000;
-    const predict = model.predict(
-      tf.tensor2d([[smallFileMB], [bigFileMB], [hugeFileMB]]),
-    );
+    const predict = model
+      .predict(tf.tensor2d([[smallFileMB], [bigFileMB], [hugeFileMB]]))
+      .toString();
     setResultComputed(`modelForPredict: ${predict}`);
   };
 
@@ -83,6 +91,13 @@ export const Predicator = () => {
           <div>{isCompute ? "Is computing..." : "To compute"}</div>
           {isCompute && <Spinner className={s.spinner} />}
         </button>
+        <button
+          className={classNames(s.button)}
+          onClick={handleTesting}
+          disabled={!model}
+        >
+          <div>Тестирование</div>
+        </button>
       </div>
       <div className={s.output}>
         <p>Result computing:</p>
@@ -91,6 +106,10 @@ export const Predicator = () => {
       <div className={s.test}>
         <p>Result test</p>
         <div className={s.result}>{resultTest}</div>
+      </div>
+      <div className={s.control_model}>
+        <p>Оценка модели на контрольных данных</p>
+        <div className={s.result}>{controlDataModel}</div>
       </div>
     </div>
   );
